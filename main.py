264e398e8f55a7714fc9cf884b13cbbe9aa14530
ROBERT Amos <robertcamos@gmail.com>
2020-09-19 05:21:49 +0000

    f

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ worksheet = workbook.add_worksheet()
 
 # Some data we want to write to the worksheet.
 expenses = (
-    ['Rent', 5000],
+    ['Rent', 6000],
     ['Gas',   100],
     ['Food',  300],
     ['Gym',    50],
